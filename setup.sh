4c3220e88652aa0096b1dc32e64dbef405b66649
Your Name <you@example.com>
2024-03-10 10:22:26 +0100

    Seventh commit

--- setup.sh
+++ setup.sh
@@ -3,10 +3,19 @@ ZELLIJ_VERSION=0.39.2
 
 # Install base stuff
 sudo apt update && sudo apt dist-upgrade
-sudo apt install -y go python3 python3-dev pipx git wget curl gpg zsh yank btop jq pgcli neofetch lsd
+sudo apt install -y apt-transport-https go python3 python3-dev pipx git wget curl gpg zsh yank btop jq pgcli neofetch lsd
 
-# Install rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# Install kubectl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmour -o /usr/share/keyrings/kubernetes.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/kubernetes.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# Install Helm
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg >/dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
 
 # Install Linux brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -21,6 +30,17 @@ brew install yakitrak/yakitrak/obs
 brew install kdabir/tap/has
 brew install ffsend
 brew install jesseduffield/lazygit/lazygit
+
+# Install Tilt
+curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | bash
+
+# Install Docker
+curl -fsSL https://get.docker.com | sh
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Install rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Install Terraform stuff
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
